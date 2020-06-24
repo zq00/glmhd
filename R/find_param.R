bf72f2fe3f35353e16aeff7033d775d246ed9ac8
Qian Zhao <qzhao1@stanford.edu>
2020-06-23 20:29:35 -0700

    add function to solve a system of equations (solutions are alpha_star, lambda_star, sigma_star, b_star depending on whether beta & gamma are zero)

---
+++ R/find_param.R
@@ -0,0 +1,66 @@
+#' Solves a system of nonlinear equations
+#'
+#' This function solves a system of equations, whose solution characterizes
+#' the asymptotic bias and variance of the M-estimator,
+#' as well as the Hessian of the loss function (in case of the MLE, it is the negative log-likelihood).
+#'
+#' @param rho_prime A function that computes the success probability \eqn{\mathrm{P}(Y=1 | X^\top \beta)},
+#'     here \eqn{\beta} is the coefficient. The default is logistic model.
+#' @param f_prime1 A function. Derivative of the loss function when \eqn{Y = 1}.
+#'     The default is the derivative of the negative log-likelihood of logistic
+#'     regression when \eqn{Y = 1}.
+#' @param f_prime0 A function. Derivative of the loss function when \eqn{Y = -1}.
+#'     The default is the derivative of the negative log-likelihood of logistic
+#'     regression when \eqn{Y = -1}.
+#' @param kappa Numeric. The problem dimension \eqn{\kappa = p/n}.
+#' @param gamma Numeric. Signal strength \eqn{\gamma = \sqrt(\mathrm{Var}(X^\top \beta))}.
+#' @param beta0 Numeric. Intercept.
+#' @param intercept If \code{TRUE}, the glm contains an intercept term.
+#'     \code{intercept = TRUE} by default.
+#' @param verbose If \code{TRUE}, print progress at each step.
+#' @return A vector solution to the system. When \code{gamma != 0} and \code{b !=0},
+#'     it is \eqn{(\alpha_\star, \lambda_\star, \sigma_\star, b_\star)}.
+#'     When signal strength is zero (\code{gamma = 0}), solutions to the system with
+#'     three equations \eqn{(\lambda_\star, \sigma_\star, b_\star)}.
+#' @importFrom pracma fsolve
+#' @include equation_binary.R prox_op.R integrate2_normal.R logistic_model.R
+#' @examples
+#' # Compute parameters for a logistic model
+#' param <- find_param(kappa = 0.1, gamma = sqrt(5))
+#' # Asymptotic bias
+#' param[1]
+#' # Standard deviation
+#' param[3] / sqrt(0.1)
+#' # Another example
+#' param <- find_param(kappa = 0.1, gamma = 0, intercept = FALSE)
+#' # Asymptotic standard deviation
+#' param[2] / sqrt(0.1)
+find_param <- function(rho_prime = rho_prime_logistic,
+                       f_prime1 = f_prime1_logistic,
+                       f_prime0 = f_prime0_logistic,
+                       kappa,
+                       gamma,
+                       beta0 = 0,
+                       intercept = TRUE,
+                       verbose = TRUE){
+  if(gamma == 0){ # case of no signal
+    if(intercept == FALSE){
+      x_init <- c(2, 2)
+    }else{
+      x_init <- c(2, 2, beta0)
+    }
+  }else if(intercept == FALSE){
+    x_init <- c(2, 2, gamma)
+  }else{
+    x_init <- c(2, 2, gamma, beta0)
+  }
+  # Setup system of equations
+  f_eq <- equation_binary(rho_prime, f_prime1, f_prime0,
+                          kappa, gamma, beta0, intercept,
+                          verbose)
+
+  sol <- fsolve(f_eq, x_init, J = NULL, maxiter = 100, tol = 1e-4,
+                        verbose)
+  sol$x
+}
+
