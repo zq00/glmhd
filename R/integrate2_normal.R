2265ed03465761ebcf5df111a0dafff8ce407322
Qian Zhao <qzhao1@stanford.edu>
2020-06-23 20:31:38 -0700

    add function - 2d normal integral

---
+++ R/integrate2_normal.R
@@ -0,0 +1,21 @@
+#' 2-dimensional Gaussian Expectation
+#'
+#' Expected value of a function of two independent
+#' standard Gaussian variables
+#'
+#' @param f Function to integrate over. Input to
+#' \code{f} should be a length 2 vector.
+#' @param ... Additional arguments to \link[cubature]{hcubature}
+#' @importFrom cubature hcubature
+#' @examples
+#' # Second moment of a Gaussian
+#' # tol = 0.0001, true value = 2, evaluated = 2.000001
+#' f <- function(x) x[1]^2 + x[2]^2
+#' integrate2_normal(f, tol = 1e-4)
+
+integrate2_normal <- function(f, ...){
+  integrand <- function(x) f(x) * dnorm(x[1]) * dnorm(x[2])
+
+  hcubature(integrand, lowerLimit = c(-8,-8), upperLimit = c(8,8),
+                      fDim = 1, maxEval = 0, tol = 1e-4,  ...)$integral
+}
