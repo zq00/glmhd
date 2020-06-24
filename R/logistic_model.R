af7665878a3d3e25ebfc8c8f061b5d1a3f80671f
Qian Zhao <qzhao1@stanford.edu>
2020-06-23 20:33:02 -0700

    add logistic model formulae

---
+++ R/logistic_model.R
@@ -0,0 +1,22 @@
+#' Logistic loss function
+#'
+#' Success probability and derivatives of the
+#' negative log-likelihood of a logistic model
+#'
+#' \code{rho_prime_logistic} computes the successs probability of
+#' a logistic model \eqn{\rho(t) = 1/(1+e^{-t})}.
+#'
+#' \code{f_prime1_logistic} and \code{f_prime0_logistic} are
+#' the derivative of the negative log-likelihood when \eqn{Y=1} and \eqn{Y=-1}.
+#'
+#' @param x Numeric. Input variable.
+
+rho_prime_logistic <- function(x) 1 / (1 + exp(-x))
+
+#' @rdname rho_prime_logistic
+f_prime1_logistic <- function(x) -1 / (1 + exp(x))
+
+#' @rdname rho_prime_logistic
+f_prime0_logistic <- function(x) 1 / (1 + exp(-x))
+
+
