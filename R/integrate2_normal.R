46de0909950c66aa8d6d5882cb043c83981f1544
Qian Zhao <qzhao1@stanford.edu>
2020-06-25 15:37:05 -0700

    do not run examples in integrate2_normal. add imported functions

--- R/integrate2_normal.R
+++ R/integrate2_normal.R
@@ -7,11 +7,14 @@
 #' \code{f} should be a length 2 vector.
 #' @param ... Additional arguments to \link[cubature]{hcubature}
 #' @importFrom cubature hcubature
+#' @importFrom stats dnorm
 #' @examples
+#' \dontrun{
 #' # Second moment of a Gaussian
 #' # tol = 0.0001, true value = 2, evaluated = 2.000001
 #' f <- function(x) x[1]^2 + x[2]^2
 #' integrate2_normal(f, tol = 1e-4)
+#' }
 
 integrate2_normal <- function(f, ...){
   integrand <- function(x) f(x) * dnorm(x[1]) * dnorm(x[2])
