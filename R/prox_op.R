2bcf0034959dbafbb805ef7791f84b4cf393c6a2
Qian Zhao <qzhao1@stanford.edu>
2020-06-25 15:38:01 -0700

    add imported functions. don't run examples for prox_op. make the interval to solve for prox-operator shorter.

--- R/prox_op.R
+++ R/prox_op.R
@@ -7,11 +7,14 @@
 #'     \deqn{\mathrm{prox}_{\lambda f}(x) = \mathrm{argmin}_x f(z) + \frac{1}{2\lambda}(z-x)^2.}
 #'     Solution to the equation
 #'     \deqn{f'(z) + \frac{1}{\lambda}(z-x) = 0.}
+#' @importFrom stats uniroot
 #' @examples
+#' \dontrun{
 #' f <- function(x) 1/(1+exp(-x))
 #' prox_op(f, 1, 1)
+#' }
 
 prox_op <- function(f_prime, lambda, x){
   f <- function(z)  z + lambda * f_prime(z) - x
-  uniroot(f, interval = c(-100,100), extendInt = "yes")$root
+  uniroot(f, interval = c(-20,20), extendInt = "yes")$root
 }
