e8bae9786b3722ac6ca136b1d3dff3a91601b5cc
Qian Zhao <qzhao1@stanford.edu>
2020-10-10 20:25:49 -0700

    update tests

--- tests/testthat/test.signal_strength.R
+++ tests/testthat/test.signal_strength.R
@@ -1,5 +1,5 @@
 context("Signal Strength")
 
 test_that("signal_strength works when either signal strength or intercept is 0", {
-  expect_equal(length(signal_strength(kappa_hat = 0.5, intercept = FALSE)), 2)
+  expect_equal(length(signal_strength(kappa_hat = 0.5, intercept = FALSE)), 1)
 })
