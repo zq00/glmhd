38004993c04afde25234b3423c9c417321d27a1d
Qian Zhao <qzhao1@stanford.edu>
2020-10-10 22:31:58 -0700

    update test

--- tests/testthat/test.solve_eq.R
+++ tests/testthat/test.solve_eq.R
@@ -2,8 +2,8 @@ context("Solve equations")
 
 test_that("find_param works when either signal strength or intercept is 0", {
   expect_equal(length(find_param(kappa = 0.1, gamma = 0, intercept = FALSE)), 3)
-  expect_equal(length(find_param(kappa = 0.1, gamma = 0, intercept = TRUE)), 3)
-  expect_equal(length(find_param(kappa = 0.1, gamma = sqrt(5), intercept = FALSE)), 4)
+  expect_equal(length(find_param(kappa = 0.1, gamma = 0, intercept = TRUE)), 4)
+  expect_equal(length(find_param(kappa = 0.1, gamma = sqrt(5), intercept = FALSE)), 3)
   expect_equal(length(find_param(kappa = 0.1, gamma = sqrt(5), intercept = TRUE)), 4)
 })
 
